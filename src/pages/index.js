2fee4e1fca917d66dc4d7d102697fc0f7b41c8e2
wuhuhu <1294538712@qq.com>
2023-12-14 19:46:29 +0800

    修改图片名及新增文档内容

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,18 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures'
 import Heading from '@theme/Heading'
 import styles from './index.module.css'
 
-import profile from '@site/static/img/profile.png'
+// import profile from '@site/static/img/profile.png'
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero ', styles.heroBanner)}>
       <div className='container'>
-        <img src={profile} style={{ width: 280, borderRadius: '50%' }} />
+        <img
+          // src={profile}
+          src='https://shapeimage.oss-cn-shenzhen.aliyuncs.com/kdlImage/01b3bee473a646b39608a2669a2af3b9-profile.png'
+          style={{ width: 280, borderRadius: '50%' }}
+        />
         <Heading as='h1' className='hero__title'>
           {siteConfig.title}
         </Heading>
